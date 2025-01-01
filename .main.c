c35cc309776f9fad9693542f12d32a82649b57c4
NaomisNuddles <nmla0012@gmail.com>
2025-01-01 02:23:23 +0000

    main ajust

--- .main.c
+++ .main.c
@@ -5,47 +5,55 @@
 
 int	main()
 {
-	int k = open("tests/random.txt", O_RDONLY);
-
-	ft_printf("fd: %d\n", k);
-	ft_printf("line1:%s$\n", get_next_line(k));
-	close(k);
-	ft_printf("line2:%s$\n", get_next_line(k));
-	k = open("tests/random.txt", O_RDONLY);
-	ft_printf("line1:%s$\n", get_next_line(k));
-	/*int 	fd[11];
-	int 	i;
+	int		fd[10];
+	char	*txt[] = {"", "tests/empty.txt", "tests/char.txt", "tests/nlx5.txt", "tests/line.txt", \
+	"tests/line_nl.txt", "tests/line_nlx2_line.txt", "tests/line_nlx2_line_nl.txt", \
+	"tests/sentences.txt", "tests/random.txt"};
+	int		i;
 	int		j;
 	char	*str;
 
-	i = 0;
+	i = 1;
 	str = 0;
 	fd[0] = -1;
-	fd[1] = open("tests/empty.txt", O_RDONLY);
-	fd[2] = open("tests/char.txt", O_RDONLY);
-	fd[3] = open("tests/nlx5.txt", O_RDONLY);
-	fd[4] = open("tests/line.txt", O_RDONLY);
-	fd[5] = open("tests/line_nl.txt", O_RDONLY);
-	fd[6] = open("tests/line_nlx2_line.txt", O_RDONLY);
-	fd[7] = open("tests/line_nlx2_line_nl.txt", O_RDONLY);
-	fd[8] = open("tests/sentences.txt", O_RDONLY);
-	fd[9] = open("tests/random.txt", O_RDONLY);
-	fd[10] = -1;
-
-	while (i <= 10)
+	while (i <= 9)
+	{
+		fd[i] = open(txt[i], O_RDONLY);
+		i++;
+	}
+	i = 0;
+	ft_printf("\033[1;34mFOR BUFFER_SIZE: %i\n\n\033[0;39m", BUFFER_SIZE);
+	while (i <= 9)
 	{
 		j = 1;
-		printf("Testing fd % i ...\n\n", fd[i]);
+		ft_printf("Testing fd % i at %s ...\n\n", fd[i], txt[i]);
 		str = get_next_line(fd[i]);
 		while (str && *str)
 		{
-			printf("	line %.1i:%s$\n", j, str);
+			ft_printf("	line %.1i:%s$\n", j, str);
 			str = get_next_line(fd[i]);
 			j++;
 		}
-		printf("	line %.1i:%s$\n\n", j, str);
-		printf("... DONE\n\n");
+		ft_printf("	line %.1i:%s$\n\n", j, str);
+		ft_printf("... DONE\n\n");
 		i++;
 	}
-	printf("\033[1;34mEND OF BUFFER_SIZE %i:\n\n\033[0;39m", BUFFER_SIZE);*/
+	i--;
+	ft_printf("Testing fd % i at %s ...\n\n", fd[i], txt[i]);
+	fd[i] = open(txt[i], O_RDONLY);
+	ft_printf("	line 1:%s$\n", get_next_line(fd[i]));
+	ft_printf("	line 2:%s$\n", get_next_line(fd[i]));
+	close(fd[i]);
+	ft_printf("> CLosed - Read\n");
+	ft_printf("	line 0:%s$\n", get_next_line(fd[i]));
+	fd[i] = open(txt[i], O_RDONLY);
+	ft_printf("	line 1:%s$\n", get_next_line(fd[i]));
+	ft_printf("	line 2:%s$\n", get_next_line(fd[i]));
+	close(fd[i]);
+	fd[i] = open(txt[i], O_RDONLY);
+	ft_printf("> CLosed - Opened - Read\n");
+	ft_printf("	line 0:%s$\n", get_next_line(fd[i]));
+	ft_printf("... DONE\n\n");
+
+	ft_printf("\033[1;34mEND OF BUFFER_SIZE: %i\n\n\033[0;39m", BUFFER_SIZE);
 }
